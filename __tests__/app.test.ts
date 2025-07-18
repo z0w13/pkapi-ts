c263e61b14337c68341b26819a5722e11c7e5670
zowie <git@zowie.cx>
2025-07-18 19:25:50 +0200

    chore(style): fix lint warnings

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -3,15 +3,13 @@ import assert from 'node:assert'
 import { add } from '../src/main.ts'
 
 describe('CLI program', () => {
-
   beforeEach(() => {
     // Reset the mocks before each test
     mock.reset()
-  });
-
-  test('Program sums two arguments', async (t) => {
-    const result = await add(1, 1);
-    assert.strictEqual(result, 2);
   })
 
-});
+  test('Program sums two arguments', async (t) => {
+    const result = await add(1, 1)
+    assert.strictEqual(result, 2)
+  })
+})
