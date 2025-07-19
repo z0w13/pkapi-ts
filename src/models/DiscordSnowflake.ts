689faf998f7fb1b9a049eb5a8b8964e4acfc5987
zowie <git@zowie.cx>
2025-07-19 12:51:07 +0200

    refactor: use branded snowflakes for guild, channel and message s

--- src/models/DiscordSnowflake.ts
+++ src/models/DiscordSnowflake.ts
@@ -8,3 +8,18 @@ const DiscordSnowflake = Schema.String.pipe(
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
 type DiscordSnowflake = Schema.Schema.Type<typeof DiscordSnowflake>
 export default DiscordSnowflake
+
+const GuildSnowflake = DiscordSnowflake.pipe(Schema.brand('GuildSnowflake'))
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
+type GuildSnowflake = Schema.Schema.Type<typeof GuildSnowflake>
+export { GuildSnowflake }
+
+const ChannelSnowflake = DiscordSnowflake.pipe(Schema.brand('ChannelSnowflake'))
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
+type ChannelSnowflake = Schema.Schema.Type<typeof ChannelSnowflake>
+export { ChannelSnowflake }
+
+const MessageSnowflake = DiscordSnowflake.pipe(Schema.brand('MessageSnowflake'))
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
+type MessageSnowflake = Schema.Schema.Type<typeof MessageSnowflake>
+export { MessageSnowflake }
