11ecd1bc26011a33a0dad369a5f29a19eb7e8541
zowie <git@zowie.cx>
2025-07-22 21:54:45 +0200

    fix(models): remove redundant checks already covered by regex

--- src/models/Birthday.ts
+++ src/models/Birthday.ts
@@ -26,27 +26,13 @@ export const BirthdayFromString = z
     const split = input.split('-')
     const [year, month, day] = [parseInt(split[0]), parseInt(split[1]), parseInt(split[2])]
 
-    if (isNaN(year)) {
-      ctx.addIssue(`Couldn't parse month: '${split[0]}'`)
+    return {
+      // NOTE: 0004 = hides the year
+      //       see https://pluralkit.me/api/models/#member-model
+      year: year === 4 ? null : year,
+      month,
+      day
     }
-
-    if (isNaN(month)) {
-      ctx.addIssue(`Couldn't parse month: '${split[1]}'`)
-    }
-
-    if (isNaN(day)) {
-      ctx.addIssue(`Couldn't parse day: '${split[2]}'`)
-    }
-
-    return ctx.issues.length > 0
-      ? z.NEVER
-      : {
-          // NOTE: 0004 = hides the year
-          //       see https://pluralkit.me/api/models/#member-model
-          year: year === 4 ? null : year,
-          month,
-          day
-        }
   })
   .pipe(Birthday)
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
