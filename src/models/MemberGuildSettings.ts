70982f62f11b2000733d79252871193201b8cdff
zowie <git@zowie.cx>
2025-07-28 14:42:48 +0200

    fix(models): split MemberGuildSettings into regular and dispatch variants

--- src/models/MemberGuildSettings.ts
+++ src/models/MemberGuildSettings.ts
@@ -3,7 +3,6 @@ import z from 'zod'
 import { GuildSnowflake } from './DiscordSnowflake.ts'
 
 const MemberGuildSettings = z.object({
-  guildId: GuildSnowflake,
   displayName: z.nullable(z.string().max(100)),
   avatarUrl: z.nullable(z.url().max(256)),
   keepProxy: z.nullable(z.boolean())
@@ -11,3 +10,10 @@ const MemberGuildSettings = z.object({
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
 type MemberGuildSettings = z.infer<typeof MemberGuildSettings>
 export default MemberGuildSettings
+
+const DispatchMemberGuildSettings = MemberGuildSettings.extend({
+  guildId: GuildSnowflake,
+})
+// eslint-disable-next-line @typescript-eslint/no-redeclare -- needed for type information
+type DispatchMemberGuildSettings = z.infer<typeof DispatchMemberGuildSettings>
+export { DispatchMemberGuildSettings }
